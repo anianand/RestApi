c7f4f495aeb8dcf5ec98a2c97c2b4a0843df214f
Animesh Anand <animesh.anand.cse15@itbhu.ac.in>
2019-01-11 21:26:41 +0530

    Adding files

---
+++ webapp/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class employees(models.Model):
+    firstName = models.CharField(max_length=10)
+    lastName = models.CharField(max_length=10)
+    emp_id = models.IntegerField()
+
+    def __str__(self):
+        return self.firstName
+
